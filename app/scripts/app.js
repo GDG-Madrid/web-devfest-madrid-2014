6e40fba98588faa8d1b2d5fc1a80f526bece6887
rormartin <rormartin@gmail.com>
2014-10-27 20:44:28 +0100

    Modificado elemento de sesión para tener más de un 'speaker' Añadida directiva para que el scroll funcione correctamente al navegar entre las sesiones desde el horario resumen Añadidas identificaciones de sesiones

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,4 +13,19 @@ angular.module('DevFestMadrid2014App', [
           when('/inscription', {templateUrl: 'views/inscription.html'}).
           when('/info-sponsors', {templateUrl: 'views/info-sponsors.html'}).
           otherwise({redirectTo: '/'});
-  });
+  })
+    .directive('scrollTo', function ($location, $anchorScroll) {
+	return function(scope, element, attrs) {
+	    element.bind('click', function(event) {
+		event.stopPropagation();
+		var off = scope.$on('$locationChangeStart', function(ev) {
+		    off();
+		    ev.preventDefault();
+		});
+		var location = attrs.scrollTo;
+		$location.hash(location);
+		$anchorScroll();
+	    }); 
+	}
+    });
+	
